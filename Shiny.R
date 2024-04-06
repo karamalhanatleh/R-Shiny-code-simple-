c0fbc4296b6a5f74e2b86455578bd456cdc656ca
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2024-04-06 22:38:01 +0300

    Add files via upload

---
+++ Shiny.R
@@ -0,0 +1,43 @@
+# # Load library
+library(shiny)
+library(shinythemes)
+
+# Define UI
+ui <-fluidPage(theme = shinytheme("cosmo"),
+               navbarPage(
+  "My first app",
+  tabPanel("Navebar 1",
+           sidebarPanel(
+             tags$h3("input"),
+             textInput("txt1","given Name: ",''),
+             textInput("txt2", "surname:",""),
+
+           ), #sidebarPanel
+           mainPanel(
+             h1("Header 1 "),
+             h3("Output 1"),
+             verbatimTextOutput("txtout"),
+           )#main panel
+           
+           ),# Navbar  1 #tabPanel 
+  tabPanel("Navbar 2 ",  "This is panel left blank"),
+  tabPanel("Navbar 3 ",  "This is panel left blank")
+)# navbar Page
+)#fluidPage
+
+server<-function()
+
+# Run the app
+shinyApp(ui, server)
+
+
+#server function
+server <-function(input,output){
+  output$txtout<-renderText({
+    paste(input$txt1 , input$txt2 ,sep = " ")
+  })
+  
+}#server
+
+#shiny object
+shinyApp(ui=ui , server=server)
---
+++ create dashboard.R
@@ -0,0 +1,27 @@
+#########################
+#Create a Simple R Shiny Dashboard 
+
+#load library
+library(shiny)
+library(shinydashboard)
+
+
+ui<-dashboardPage(
+  dashboardHeader(title = "My dashboard"),
+  dashboardSidebar(),
+  dashboardBody(
+    box(plotOutput("corr_plot") , width = 10)
+    
+  )
+)
+
+
+
+server<-function(input,output){
+  output$corr_plot <-renderPlot({
+    plot(iris$Sepal.Length , iris$Petal.Length)
+    
+  })
+}
+
+shinyApp(ui , server)
